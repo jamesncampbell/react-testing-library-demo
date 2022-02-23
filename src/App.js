79e2a5b9e3b4b49854b8d19e27ca6ecc3ffdd244
James Campbell <jcampbell@ellevest.com>
2022-02-23 09:28:50 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+export const salary = 80000
+export const oneMonthTakeHome = Math.round(salary * .65 / 12)
+export const twoMonthTakeHome = Math.round(salary * .65 / 6)
+export const threeMonthTakeHome = Math.round(salary * .65 / 4)
+
+const App = () => {
+  const handleSubmit = () => {
+    console.warn('hello')
+  }
+
+  const initialState = { oneTimeDeposit: twoMonthTakeHome, isValid: true }
+
+  const reducer = (state, action) => {
+    switch(action.type) {
+      case 'oneMonthTakeHome': {
+        return { ...state, oneTimeDeposit: oneMonthTakeHome }
+      }
+      case 'twoMonthTakeHome': {
+        return { ...state, oneTimeDeposit: twoMonthTakeHome }
+      }
+      case 'threeMonthTakeHome': {
+        return { ...state, oneTimeDeposit: threeMonthTakeHome }
+      }
+      case 'customTakeHome': {
+        return { 
+          ...state,
+          oneTimeDeposit: action.payload,
+          isValid: Number.isInteger(parseInt(action.payload)) && (parseInt(action.payload) === 0 || parseInt(action.payload) > 0)
+        }
+      }
+      default: {
+        return initialState
+      }
+    }
+  }
+
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="PageHeader">Would you like to set a one-time deposit?</h1>
       </header>
+        <form>
+          <label className="OneTimeDepositLabel" htmlFor="oneTimeDeposit">One time deposit amount</label>
+          <input 
+            id="oneTimeDeposit"
+            name="oneTimeDeposit" 
+            className="OneTimeDepositInputField"
+            type="number" 
+            value={state.oneTimeDeposit} 
+            onChange={event => dispatch({ type: 'customTakeHome', payload: event.target.value })}
+          />
+        </form>
+        <ul className="TakeHomePayButtons">
+          <li>
+            <button 
+              data-testid="oneMonthTakeHomeButton" 
+              onClick={() => dispatch({ type: 'oneMonthTakeHome' })}>
+                1 month take-home pay
+            </button>
+          </li>
+          <li>
+            <button 
+              data-testid="twoMonthTakeHomeButton"
+              onClick={() => dispatch({ type: 'twoMonthTakeHome' })}>
+              2 months take-home pay
+            </button>
+          </li>
+          <li>
+            <button 
+              data-testid="threeMonthTakeHomeButton"
+              onClick={() => dispatch({ type: 'threeMonthTakeHome' })}>
+              3 months take-home pay
+            </button>
+          </li>
+        </ul>
+        {!state.isValid && 
+          <p 
+            data-testid="errorMessage" 
+            className="ErrorMessage">
+              Oops, you must enter a valid dollar amount or $0
+          </p>
+        }
+        <button disabled={!state.isValid} className="SubmitButton" onClick={handleSubmit}>
+          Submit
+        </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import App, { oneMonthTakeHome, twoMonthTakeHome, threeMonthTakeHome } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const setup = () => {
+}
+
+test('it renders with an enabled button', () => {
+})
+
+test('it renders with the correct amount', () => {
+})
+
+test('clicking the oneMonthTakeHome button shows the correct amount in the input field', () => {
+})
+
+test('clicking the twoMonthTakeHome button shows the correct amount in the input field', () => {
+})
+
+test('clicking the threeMonthTakeHome button shows the correct amount in the input field', () => {
+})
+
+test('it allows the user to enter $0', () => {
+})
+
+test('it disables the submit button if the user enters a non-positive integer', () => {
+})
+
+test('it shows helper text if the user enters an error state', () => {
+})
+
